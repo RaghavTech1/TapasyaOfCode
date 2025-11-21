d72698044ffd60766ec59f9a8f920f5392d3cc10
RaghavTech1 <raghavkohli1609@gmail.com>
2025-11-21 18:06:29 +0530

    Selections2

---
+++ Selections/SimpleIfDemo.java
@@ -0,0 +1,19 @@
+package Selections;
+
+import java.util.Scanner;
+
+public class SimpleIfDemo {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an Integer: ");
+        int number = input.nextInt();               //enter input
+
+        if(number % 5 == 0){                        //check 5
+            System.out.println("HiFive");
+        }
+        if(number % 2 == 0){                        //check even
+            System.out.println("HiEven");
+        }
+        input.close();
+    }
+}
