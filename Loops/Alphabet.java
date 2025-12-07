848c3d8b15242fb57b632c74501e929e8e94dc66
RaghavTech1 <raghavkohli1609@gmail.com>
2025-12-07 18:18:09 +0530

    loop2

---
+++ Loops/Alphabet.java
@@ -0,0 +1,12 @@
+package Loops;
+
+public class Alphabet {
+    public static void main(String[] args) {
+        char name;
+
+        for(name = 'a'; name <= 'z';name++){
+            System.out.print(name);
+        }
+        return;
+    }
+}
