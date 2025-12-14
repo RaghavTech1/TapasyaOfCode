a4bababb0d501f26a34442c892b92f21807370ef
RaghavTech1 <raghavkohli1609@gmail.com>
2025-12-14 08:31:10 +0530

    Method4

---
+++ Methods/Increment.java
@@ -0,0 +1,15 @@
+package Methods;
+
+public class Increment {
+    public static void main(String[] args) {
+        int x = 1;
+        System.out.println("Before the call, x is " + x);
+        increment(x);
+        System.out.println("After the call, x is " + x);
+    }
+
+    public static void increment(int n) {
+        n++;
+        System.out.println("n is inside the method is " + n);
+    }
+}
